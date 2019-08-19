7877ef751fd861bc6ade4aa5027b16d0a348eeec
python060319 <51337287+python060319@users.noreply.github.com>
2019-08-19 20:06:42 +0300

    Update json_1.py

--- json_1.py
+++ json_1.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 req = requests.get\
-    ("https://jsonplaceholder.typicode.com/users/1")
+    ("https://jsonplaceholder.typicode.com/users")
 print(f'HTTP Status code: {req.status_code}')
 print(f'HTTP Header: {req.headers}')
 json_r = json.loads(req.content)
@@ -11,14 +11,36 @@ print(json_r)
 class User:
     def __init__(self, jsonObj):
         self.__dict__ = jsonObj
-    def __str__(self):
-        pass
+    def __rep__(self):
+        res = ''
+        for k,v in\
+                self.__dict__.items():
+            res += f'{k} : {v} '
+        return res
 
-u = User(json_r)
-print(u.name)
-print(u.id)
+#u = User(json_r)
+#print(u.name)
+#rint(u.id)
 
-# read all users with get
-# create objects for all of the users, create a list from all of  the users
-# implement __str__
-# print the list
+# GET - brings data (all)
+# GET / id - brings specific id
+# POST - add
+# PUT - update
+# DELETE
+
+data = {'title' : 'Python request',
+        'body' : 'My request body',
+        'userId' : 1}
+result = requests.post("https://jsonplaceholder.typicode.com/users",
+              data)
+print(result.status_code)
+print(result.text)
+
+result = requests.put("https://jsonplaceholder.typicode.com/users/1",
+              data)
+print(result.status_code)
+print(result.text)
+
+result = requests.delete("https://jsonplaceholder.typicode.com/users/1")
+print(result.status_code)
+print(result.text)
